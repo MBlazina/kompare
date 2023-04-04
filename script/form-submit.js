844980ee67e8818ac7e68f1a30c91f0a4bf88810
MBlazina <mirko.blazina@gmail.com>
2023-04-04 14:45:22 +0200

    fix: form submit function

--- script/form-submit.js
+++ script/form-submit.js
@@ -1,5 +1,11 @@
-/* const submitForm = () => {
-  alert("form submit event")
-}
 
-const submitButton = document.getElementById("submit") */
+
+/* document.addEventListener("DOMContentLoaded", function () { */
+
+const submitButton = document.getElementById("submit")
+
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault()
+  alert("form submit event")
+})
+/* }) */
