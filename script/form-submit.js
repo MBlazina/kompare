0938facc8112b0d683d940dad9f876721c2318df
MBlazina <mirko.blazina@gmail.com>
2023-04-05 02:22:08 +0200

    feat: footer done

--- script/form-submit.js
+++ script/form-submit.js
@@ -1,11 +1,6 @@
-
-
-/* document.addEventListener("DOMContentLoaded", function () { */
-
 const submitButton = document.getElementById("submit")
 
 submitButton.addEventListener("click", function (e) {
   e.preventDefault()
   alert("form submit event")
 })
-/* }) */
